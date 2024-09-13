b04a1fe8f069ffe6a800936fb253a5b52713544e
JahirMM <jahirmachuca45@gmail.com>
2024-09-12 21:41:50 -0300

    style: ajustar el tamaño de las letras y colores en la sección de experiencia

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -38,25 +38,27 @@ const Experience = () => {
               onClick={() => handleClick(company.company)}
               className={`cursor-pointer transition-all duration-300 relative ${
                 companySelected === company.company
-                  ? "text-white font-bold pb-2 border-b-2 border-secondaryColour md:border-l-2 md:border-b-0 md:pl-2 md:pb-0"
-                  : "text-gray-500/80 pb-2 border-b-2 border-gray-600/80 md:border-l-2 md:border-b-0 md:pl-2 md:pb-0"
+                  ? "text-white text-xl font-bold pb-2 border-b-2 border-secondaryColour md:border-l-2 md:border-b-0 md:pl-4 md:pb-0"
+                  : "text-gray-500/80 text-lg pb-2 border-b-2 border-gray-600/80 md:border-l-2 md:border-b-0 md:pl-4 md:pb-0"
               } z-40`}
             >
               {company.company}
             </h4>
           ))}
-          <span className="pedo z-30 w-full h-[2px] absolute bottom-0 bg-gray-600/80 md:left-0 md:w-[2px] md:h-full"></span>
+          <span className="pedo z-30 w-full h-[2px] absolute bottom-0 bg-gray-600 md:left-0 md:w-[2px] md:h-full"></span>
         </span>
       </nav>
       <div className="md:col-start-2 md:col-end-4">
         {EXPERIENCE.filter((item) => item.company === companySelected).map(
           (experience, index) => (
             <div key={index}>
-              <h3 className="text-secondaryColour">{experience.title}</h3>
-              <time className="p-0 m-0 text-sm text-gray-600/80 dark:text-white/80">
+              <h3 className="text-secondaryColour text-2xl text-pretty">
+                {experience.title}
+              </h3>
+              <time className="p-0 m-0 text-sm text-gray-400 dark:text-white/80">
                 {experience.startDate} - {experience.endDate}
               </time>
-              <p className="mt-5">{experience.description}</p>
+              <p className="mt-5 text-gray-300">{experience.description}</p>
             </div>
           )
         )}
