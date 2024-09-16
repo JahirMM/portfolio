ade3464682c62bf7da59df1fa58a6d3a2c770416
JahirMM <jahirmachuca45@gmail.com>
2024-09-16 02:41:28 -0300

    feat: agregar funcionalidad para copiar el correo electrónico

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,26 @@
-import Github from "../icons/Github";
+import { useState } from "react";
 import LinkedIn from "../icons/LinkedIn";
-import Mail from "../icons/Mail";
 import SocialLink from "./SocialLink";
+import Github from "../icons/Github";
+import Mail from "../icons/Mail";
+import Copy from "../icons/Copy";
 
 const Hero = () => {
+  const [copied, setCopied] = useState(false);
+  const textToCopy = "jahirmachuca45@gmail.com";
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 1200);
+    } catch (err) {
+      console.error("Error al copiar el texto: ", err);
+    }
+  };
+
   return (
     <div className="grid grid-cols-1 gap-10">
       <div className="lg:col-start-1 lg:col-end-3">
@@ -34,10 +51,27 @@ const Hero = () => {
             <Github className="size-5" />
             GitHub
           </SocialLink>
-          <SocialLink href="mailto:jahirmachuca45@gmail.com">
-            <Mail className="size-5" />
+          <div className="bg-gray-200/50 rounded-full border border-black/20 flex justify-center items-center gap-x-2 py-2 px-4 hover:bg-white/30 dark:hover:bg-white/10 transition dark:border-white/10 dark:bg-white/5">
+            <a
+              href="mailto:jahirmachuca45@gmail.com"
+              className=""
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Mail className="size-5" />
+            </a>
+            <span className="relative">
+              <Copy className="size-5 cursor-pointer" onClick={handleCopy} />
+              <span
+                className={`bg-white/85 text-black text-xs px-2 py-1 rounded-xl absolute transition-opacity duration-500 top-0 translate-y-[-37px] left-[-20px] ${
+                  copied ? "opacity-100" : "opacity-0"
+                } border border-gray-300`}
+              >
+                copiado
+              </span>
+            </span>
             Contáctame
-          </SocialLink>
+          </div>
         </nav>
       </div>
       <div className="flex justify-center lg:col-start-3 lg:col-end-4">
