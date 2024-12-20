f968790755e7d1e001777f6b85475180dea10227
JahirMM <jahirmachuca45@gmail.com>
2024-12-20 09:49:18 -0300

    style: Actualizar diseño del menú de proyectos backend

--- src/components/projects/projectDetails/backend/BackendProjectNavigation.tsx
+++ src/components/projects/projectDetails/backend/BackendProjectNavigation.tsx
@@ -14,75 +14,111 @@ interface NavigationOption {
 interface ProjectNavigationProps {
   navigationOptions: NavigationOption[];
   onNavigate: (moduleUrl: string) => void;
-  nameModule: string
-
+  nameModule: string;
 }
 
 function BackendProjectNavigation({
   navigationOptions,
   onNavigate,
-  nameModule
+  nameModule,
 }: ProjectNavigationProps) {
-  
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
 
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <>
-      <span
-        className="fixed top-[32px] right-[50px] z-[99] cursor-pointer md:hidden"
-        onClick={() => setShowMenu(!showMenu)}
-      >
-        {showMenu ? (
-          <IconX className="size-7 text-black dark:text-white" />
-        ) : (
-          <Menu className="size-7 text-black dark:text-white" />
-        )}
-      </span>
-      <div
-        className={`
-            bg-cardsBackgroundLightTheme
-            fixed top-0 z-50
-            w-full p-8
-            transition-transform duration-700
-            flex flex-col items-center
-            ${showMenu ? "-translate-y-0" : "-translate-y-full"}
-            md:-translate-y-0
-            md:fixed
-            md:h-full
-            md:w-[185px]
-            md:pt-7
-            lg:pt-10
-            dark:bg-cardsBackgroundDarkTheme
-        `}
-      >
-        <h2 className="text-2xl font-bold mb-4 text-pretty text-gray-800 dark:text-white">
-          Rest API
-        </h2>
-        <ul className="flex flex-col items-center gap-4 w-full">
-          {navigationOptions.map((navOption) => (
-            <li key={navOption.url} className={`text-center w-full rounded-md ${nameModule === navOption.url ? "bg-[#E3D5C5]/50 dark:bg-[#142053]" : ""}`}>
-              <button
-                onClick={() => {
-                  onNavigate(navOption.url)
-                  setShowMenu((showMenu) => !showMenu)
-                }}
-                className="text-gray-700 px-3 py-1 rounded-xl dark:opacity-80 dark:text-gray-300 hover:text-black hover:font-medium dark:hover:text-white"
-              >
-                {navOption.nameOption}
-              </button>
-            </li>
-          ))}
-          <span className="h-[1px] w-full bg-gray-500/30"></span>
-          <ThemeSwitcher />
-          <button
-            onClick={() => navigate("/")}
-            className="text-black dark:opacity-80 dark:text-white"
+      <div className="fixed top-[27px] right-[38px] z-[999]">
+        <div
+          className="relative z-[999] cursor-pointer md:hidden"
+          onClick={toggleMenu}
+        >
+          {showMenu ? (
+            <IconX
+              className={`min-h-9 min-w-9 p-1 bg-cardsBackgroundLightTheme text-black dark:bg-[#0D1228] dark:text-white ${
+                showMenu ? "rounded-tr-xl" : "rounded-full"
+              }`}
+            />
+          ) : (
+            <Menu
+              className={`min-h-9 min-w-9 p-1 bg-cardsBackgroundLightTheme text-black dark:bg-[#0D1228] dark:text-white ${
+                showMenu ? "rounded-tr-xl" : "rounded-full"
+              } `}
+            />
+          )}
+        </div>
+        <div
+          className={`
+          options absolute top-0 right-0 w-0 h-9 overflow-hidden overflow-y-auto no-scrollbar
+          bg-cardsBackgroundLightTheme
+          text-black 
+          rounded-xl
+          z-[99] p-4
+          md:fixed md:min-h-full md:w-[185px] md:left-0 md:right-auto md:rounded-none md:pt-7
+          lg:pt-10
+          dark:bg-[#0D1228]
+          ${
+            showMenu
+              ? "w-[200px] h-[248px] max-h-[348px] open"
+              : "w-0 h-9 closed"
+          }`}
+        >
+          <h2
+            className={`text-xl text-center font-bold mb-4 text-pretty text-gray-800 md:text-2xl dark:text-white ${
+              showMenu
+                ? "opacity-100 transition-opacity duration-[2s]"
+                : "opacity-0"
+            } md:opacity-100`}
           >
-            {" "}
-            Inicio{" "}
-          </button>
-        </ul>
+            Rest API
+          </h2>
+          <ul
+            className={`
+              flex flex-col items-center gap-4 w-full
+              ${
+                showMenu
+                  ? "opacity-100 transition-opacity duration-[2s]"
+                  : "opacity-0"
+              }
+              md:opacity-100`}
+          >
+            {navigationOptions.map((navOption) => (
+              <li
+                key={navOption.url}
+                className={`
+                  text-center w-full rounded-md 
+                  ${
+                    nameModule === navOption.url
+                      ? "bg-[#E3D5C5]/50 dark:bg-[#142053]"
+                      : ""
+                  }
+                  `}
+              >
+                <button
+                  onClick={() => {
+                    onNavigate(navOption.url);
+                    toggleMenu;
+                  }}
+                  className="text-gray-700 px-3 py-1 rounded-xl dark:opacity-80 dark:text-gray-300 hover:text-black hover:font-medium dark:hover:text-white"
+                >
+                  {navOption.nameOption}
+                </button>
+              </li>
+            ))}
+            <span className="h-[1px] w-full bg-gray-500/30"></span>
+            <ThemeSwitcher />
+            <button
+              onClick={() => navigate("/")}
+              className="text-black dark:opacity-80 dark:text-white"
+            >
+              {" "}
+              Inicio{" "}
+            </button>
+          </ul>
+        </div>
       </div>
     </>
   );
--- src/components/projects/projectDetails/backend/ModuleDetails.tsx
+++ src/components/projects/projectDetails/backend/ModuleDetails.tsx
@@ -1,13 +1,18 @@
-
 import { Module } from "@/interfaces/backendInterfaces";
 import ApiDetails from "@/components/projects/projectDetails/backend/ApiDetails";
+import { useState } from "react";
+import Menu from "@/icons/Menu";
+import IconX from "@/icons/IconX";
 
 interface ModuleDetailsProps {
   module: Module;
   generateId: (title: string) => string;
 }
 
-const ModuleDetails: React.FC<ModuleDetailsProps> = ({ module, generateId  }) => {
+const ModuleDetails: React.FC<ModuleDetailsProps> = ({
+  module,
+  generateId,
+}) => {
   if (
     (!module.title || module.title.trim() === "") &&
     (!module.description || module.description.trim() === "") &&
@@ -16,10 +21,45 @@ const ModuleDetails: React.FC<ModuleDetailsProps> = ({ module, generateId  }) =>
     return null;
   }
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="module-details mb-8">
-      {module.title && <h2 className="text-gray-800 text-3xl font-bold mb-10 pb-4 border-b border-gray-500/30 dark:text-gray-300">{module.title}</h2>}
-      {module.description && <p className="text-gray-800 text-sm text-pretty mb-10 dark:text-gray-300">{module.description}</p>}
+      {/* <div className="menu-container">
+        <div className="icons">
+          {isOpen ? (
+            <IconX
+              className="size-7 text-black dark:text-red-400 icon"
+              onClick={toggleMenu}
+            />
+          ) : (
+            <Menu
+              className="size-7 text-black dark:text-blue-400 icon"
+              onClick={toggleMenu}
+            />
+          )}
+        </div>
+        <div className={`options ${isOpen ? "open" : "closed"}`}>
+          <p>Opción 1</p>
+          <p>Opción 2</p>
+          <p>Opción 3</p>
+        </div>
+      </div> */}
+
+      {module.title && (
+        <h2 className="text-gray-800 text-3xl font-bold mb-10 pb-4 border-b border-gray-500/30 dark:text-gray-300">
+          {module.title}
+        </h2>
+      )}
+      {module.description && (
+        <p className="text-gray-800 text-sm text-pretty mb-10 dark:text-gray-300">
+          {module.description}
+        </p>
+      )}
       {module.apis &&
         module.apis.map((api, index) => (
           <ApiDetails key={index} api={api} generateId={generateId} />
