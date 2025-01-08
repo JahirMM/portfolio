d09975c31e22d95d02d17e2747abdc21811e70e2
JahirMM <jahirmachuca45@gmail.com>
2025-01-08 17:43:55 -0300

    fix: arreglar la descarga del CV

--- src/components/DownloadCV.tsx
+++ src/components/DownloadCV.tsx
@@ -3,7 +3,7 @@ import Download from "@/icons/Download";
 function DownloadCV() {
   return (
     <a
-      href="/src/data/CV/CV-jahir-jafet-machuca-martinez.pdf"
+      href="/CV/CV-jahir-jafet-machuca-martinez.pdf"
       download="CV-Jahir-Jafet-Machuca-Martinez.pdf"
       className="text-sm flex items-center gap-3 bg-gray-200/50 text-gray-800 rounded-full border border-black/20 py-[2px] px-3 dark:border-white/10 dark:bg-white/5 dark:text-white"
     >
